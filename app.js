12ca99bab47b38d50fa05a88da9a1e8ec28ac745
monuyadav016 <monuyadav016@gmail.com>
2020-03-08 10:24:34 +0530

    Session handling using express-session

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 const dialogflow = require('dialogflow');
 const uuid = require('uuid');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const express = require('express');
 
 const app = express();
 const port = 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(cookieParser());
+app.use(session({
+  secret: "IypDafhYtUFxWiJt1-DSQpvi",
+  saveUninitialized: true,
+  resave: false,
+  cookie: {
+      path: "/",
+      maxAge:  1800000,  //30 mins
+      // httpOnly: true
+  }
+}));
 
-// A unique identifier for the given session
-const sessionId = uuid.v4();
+app.get('/', (req, res) => {
+  // A unique identifier for the given session 
+  if(req.session.uuid)
+  {
+
+  } else {
+    req.session.uuid = uuid.v4();
+  }
+  // console.log("Session ID : " + req.session.uuid);
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+})
 
 app.post('/send-msg', (req, res) => {
-  runDialog(req.body.message).then(data => {
+  runDialog(req.body.message, req.session.uuid).then(data => {
     res.send({reply : data})
   })
 })
@@ -25,9 +48,7 @@ app.post('/send-msg', (req, res) => {
  * Send a query to the dialogflow agent, and return the query result.
  * @param {string} projectId The project to be used
  */
-async function runDialog(message, projectId = 'college-bot-82081') {
-  
-
+async function runDialog(message, sessionId, projectId = 'college-bot-82081') {
   // Create a new session
   const sessionClient = new dialogflow.SessionsClient({
     keyFilename : "/home/monu/Desktop/Projects/Chatbot/college-bot-9a2ca19f825b.json"
@@ -49,18 +70,21 @@ async function runDialog(message, projectId = 'college-bot-82081') {
 
   // Send request and log result
   const responses = await sessionClient.detectIntent(request);
-  console.log('Detected intent');
+  // console.log('Detected intent');
   const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-  if (result.intent) {
-    console.log(`  Intent: ${result.intent.displayName}`);
-  } else {
-    console.log(`  No intent matched.`);
-  }
+  // console.log(`  Query: ${result.queryText}`);
+  // console.log(`  Response: ${result.fulfillmentText}`);
+  // if (result.intent) {
+  //   console.log(`  Intent: ${result.intent.displayName}`);
+  // } else {
+  //   console.log(`  No intent matched.`);
+  // }
 
   return result.fulfillmentText;
 }
+
+// Using it above GET request to "/" does not execute the connected function only serves the index.html file
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log('Running on port '+port);
